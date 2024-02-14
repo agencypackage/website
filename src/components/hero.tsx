3099397bde7e1c2c333b9da359be3093b3fa4f87
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-02-14 03:19:52 +0100

    gk

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
             <div className="text-white w-[75%] mx-auto text-center">
             <SectionStarter section="Book a 100% non-binding discovery call to learn more about your opportunities" />
                 <h1 className="text-[70px] leading-[70px] w-[80%] font-[500] mx-auto mb-[20px]">We scale tomorrow's online  <span className="bg-gradient-to-r from-blue-500 via-blue-400 via-blue-300 via-blue-400 to-blue-500 inline-block text-transparent bg-clip-text">education companies</span></h1>
-                <p className="text-[20px] text-gray-200 leading-[30px] w-[60%] mx-auto mb-[40px]">Let’s fill your calendar with qualified prospects on a pay-for-results basis by leveraging our deep industry know-how and a results-first philosophy.</p>
+                <p className="text-[20px] text-gray-200 leading-[30px] w-[60%] mx-auto mb-[40px]">Let's fill your calendar with qualified prospects on a pay-for-results basis by leveraging our deep industry know-how and a results-first philosophy.</p>
                 <div className="flex items-center justify-center gap-[30px] mb-[80px]">
                 <div className="flex items-center gap-[8px] text-[20px]"><img className="w-[22px]" src="/checklist.png"></img>Fuld vækst</div>
                 <div className="flex items-center gap-[8px] text-[20px]"><img className="w-[22px]" src="/checklist.png"></img>Performance</div>
